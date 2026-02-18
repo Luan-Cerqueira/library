68296fcabcfedc35710edb6f46434be3af47b335
luan <luancerqueira.s2@gmail.com>
2026-02-18 10:43:07 -0300

    refactor: utilizando classe do tipo Record para montar a resposta de erro

--- src/main/java/dev/luanc/library/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/luanc/library/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package dev.luanc.library.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,62 +10,45 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Object> methodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request){
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST.value());
-        body.put("description", request.getDescription(false));
-        body.put("message", ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(error -> error.getDefaultMessage())
-                .toList());
-
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> methodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+        ErrorMessage errorBody = new ErrorMessage(LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                request.getDescription(false),
+                ex.getBindingResult().getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList());
+        return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST.value());
-        body.put("description", request.getDescription(false));
-        body.put("message", ex.getMessage());
+    public ResponseEntity<Object> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorMessage errorBody = new ErrorMessage(LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                request.getDescription(false),
+                ex.getMessage());
 
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotActiveException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> userNotActiveException(UserNotActiveException ex, WebRequest request){
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST.value());
-        body.put("description", request.getDescription(false));
-        body.put("message", ex.getMessage());
+    public ResponseEntity<Object> userNotActiveException(UserNotActiveException ex, WebRequest request) {
+        ErrorMessage errorBody = new ErrorMessage(LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                request.getDescription(false),
+                ex.getMessage());
 
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BookCopyNotAvailableException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> bookCopyNotAvailableException(BookCopyNotAvailableException ex, WebRequest request){
-        /*
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST.value());
-        body.put("description", request.getDescription(false));
-        body.put("message", ex.getMessage());
-         */
+    public ResponseEntity<Object> bookCopyNotAvailableException(BookCopyNotAvailableException ex, WebRequest request) {
         ErrorMessage errorBody = new ErrorMessage(LocalDateTime.now(),
                 HttpStatus.BAD_REQUEST.value(),
                 request.getDescription(false),
